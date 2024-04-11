8a9df0af1f906bbcf3907b75ee2aa21a3cdef644
BlankSDD <164851483+BlankSDD@users.noreply.github.com>
2024-04-11 16:09:40 +0800

    Update 1701.py

--- 1701.py
+++ 1701.py
@@ -1,7 +1,7 @@
 # 1701. 平均等待时间
 
 class Solution:
-    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+    def averageWaitingTime(self, customers):
         wait = 0
         cur = 0
         for arr,cook in customers:
